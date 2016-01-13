963d1904ce0acdeca3b5aaca8dd246214e0ef063
fufukwang <fu@thenewslens.com>
2016-01-13 16:51:58 +0800

    change style

--- js/app.js
+++ js/app.js
@@ -2,17 +2,14 @@
 
 var chatApp = angular.module("chatApp", ["firebase"]);
 
-// http is build in...  but if we use, will put firebase below AND above because it is external.
-
 chatApp.controller('ChatController', ['$scope', '$firebase', function ($scope, $firebase) {
 	var chatRef = new Firebase("https://popping-heat-4409.firebaseio.com/chats");
   $scope.comments = $firebase(chatRef);
 
   $scope.addComment = function(comment,name) {
-    // AngularFire $add method
+    
     $scope.comments.$add({comment: comment, when: Date(), name: name });
-    //or add a new person manually
-    // chatRef.update({name: 'Alex', age: 35});
+
     $scope.newComment = "";
     $scope.newName = "";
   };
